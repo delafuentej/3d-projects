b990923af378cea7296fd85d3b9431bd1c3d9381
Jesús de la Fuente <jfuentelucas@gmail.com>
2026-04-09 19:04:05 +0200

    visuals improvement

--- src/main.js
+++ src/main.js
@@ -1,24 +1,58 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+import projects from "./data.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
+const app = document.querySelector("#app");
+
+app.innerHTML = `
+  <header>
+    <h1>My Creative Projects</h1>
+    <p class="subtitle">
+      A collection of creative visual explorations using
+      <strong>Three.js</strong>,
+      <strong>React Three Fiber</strong>,
+      <strong>GLSL</strong> & GSAP.
     </p>
-  </div>
+  </header>
+
+  <main class="scrollable">
+    <div class="projects-grid" id="projects"></div>
+  </main>
+
+  <footer>
+    <p>Made by Jesús de la Fuente</p>
+    <p>&copy; 2025 All rights reserved.</p>
+  </footer>
 `;
 
-setupCounter(document.querySelector("#counter"));
+const container = document.querySelector("#projects");
+
+// 🔥 render dinámico de TODOS tus proyectos
+container.innerHTML = projects
+  .map(
+    (project) => `
+      <a class="grid-item" href=${project.url} target="_blank">
+        
+        <div 
+          class="grid-item-img"
+          style="background-image: url(${project.img})"
+        ></div>
+
+        <div class="grid-item-content">
+  <h3 class="grid-item-title">
+    ${project.title}
+    ${
+      project.tags
+        ? `<span class="grid-item-tags">
+            ${project.tags
+              .split(" · ")
+              .map((tag) => `<span class="grid-item-tag">${tag}</span>`)
+              .join("")}
+          </span>`
+        : ""
+    }
+  </h3>
+</div>
+
+      </a>
+    `,
+  )
+  .join("");
---
+++ src/utils/index.js
@@ -0,0 +1,91 @@
+import p1 from "/images/projects/p1.webp";
+import p2 from "/images/projects/p2.webp";
+import p3 from "/images/projects/p3.webp";
+import p4 from "/images/projects/p4.webp";
+import p5 from "/images/projects/p5.webp";
+import p6 from "/images/projects/p6.webp";
+import p7 from "/images/projects/p7.webp";
+import p8 from "/images/projects/p8.webp";
+import p9 from "/images/projects/p9.webp";
+import p10 from "/images/projects/p10.webp";
+import p11 from "/images/projects/p11.webp";
+import p12 from "/images/projects/p12.webp";
+import p13 from "/images/projects/p13.webp";
+import p14 from "/images/projects/p14.webp";
+import p15 from "/images/projects/p15.webp";
+import p16 from "/images/projects/p16.webp";
+import p17 from "/images/projects/p17.webp";
+import p18 from "/images/projects/p18.webp";
+import p19 from "/images/projects/p19.webp";
+import p20 from "/images/projects/p20.webp";
+import p21 from "/images/projects/p21.webp";
+import p22 from "/images/projects/p22.webp";
+import p23 from "/images/projects/p23.webp";
+import p24 from "/images/projects/p24.webp";
+import p25 from "/images/projects/p25.webp";
+import p26 from "/images/projects/p26.webp";
+import p27 from "/images/projects/p27.webp";
+import p28 from "/images/projects/p28.webp";
+import p29 from "/images/projects/p29.webp";
+import p30 from "/images/projects/p30.webp";
+import p31 from "/images/projects/p31.webp";
+import p32 from "/images/projects/p32.webp";
+import p33 from "/images/projects/p33.webp";
+import p34 from "/images/projects/p34.webp";
+import p35 from "/images/projects/p35.webp";
+import p36 from "/images/projects/p36.webp";
+import p37 from "/images/projects/p37.webp";
+import p38 from "/images/projects/p38.webp";
+import p39 from "/images/projects/p39.webp";
+import p40 from "/images/projects/p40.webp";
+import p41 from "/images/projects/p41.webp";
+import p42 from "/images/projects/p42.webp";
+import p43 from "/images/projects/p43.webp";
+import p44 from "/images/projects/p44.webp";
+
+export {
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+  p16,
+  p17,
+  p18,
+  p19,
+  p20,
+  p21,
+  p22,
+  p23,
+  p24,
+  p25,
+  p26,
+  p27,
+  p28,
+  p29,
+  p30,
+  p31,
+  p32,
+  p33,
+  p34,
+  p35,
+  p36,
+  p37,
+  p38,
+  p39,
+  p40,
+  p41,
+  p42,
+  p43,
+  p44,
+};
